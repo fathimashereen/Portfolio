0668ea26da88f21d99835efaf9f823323bfe4698
Fathima Shereen <fshereen5016@gmail.com>
2024-10-24 02:30:33 +0400

    updated path

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -6,7 +6,7 @@ import { Animate } from 'react-simple-animate'
 const Home = () => {
   const navigate=useNavigate()
   const handleNavigateToContactMePage =()=>{
-    navigate('Portfolio/contact')
+    navigate('./contact')
   }
   
   return (
